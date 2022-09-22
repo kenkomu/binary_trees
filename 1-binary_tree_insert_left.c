09c11e1331a0c2d2b16aa0b1c38207b8c4e07e47
kenkomu <komukenneth216@gmail.com>
2022-09-22 22:46:58 +0300

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,12 +8,16 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new =  malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
+	if (parent == NULL)
+		return (NULL);
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
-	new->parent = parent;
-	new->n = value;
-	new->left = new->right = NULL;
+	new->left = parent->left;
+	if (new->left)
+	new->left->parent = new;
+	parent->left = new;
 	return (new);
 }

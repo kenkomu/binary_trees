f1fc86dc8c9536aad7e3518d20d47ec716cb284d
kenkomu <komukenneth216@gmail.com>
2022-09-22 23:12:38 +0300

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,7 +7,7 @@
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return(NULL)
+		return (NULL);
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->left);
 	free(tree);

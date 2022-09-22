6a9b9e8a14d6e79ed1930964c046584653f8cd3b
kenkomu <komukenneth216@gmail.com>
2022-09-22 23:36:19 +0300

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,13 +3,14 @@
  * binary_tree_is_leaf-checks if a node is a leaf
  *
  * @node: node to check
- * Return: int
+ * Return: SUCCESS:1
+ *         FAILED: 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node.left == NULL && node.right == NULL)
+	if (node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
 }

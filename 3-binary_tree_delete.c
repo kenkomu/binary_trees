9f0826ab32517f7e1f8e87816bc44f0e38af34b7
kenkomu <komukenneth216@gmail.com>
2022-09-22 23:03:23 +0300

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete-deletes an entire binary tree
+ *
+ * @tree:tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (NULL);
+	free *tree;
+
+	return (0);
+}

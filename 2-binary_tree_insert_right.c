9afe268b0492907451b1d078188ba3b04c8f454a
kenkomu <komukenneth216@gmail.com>
2022-09-22 22:49:44 +0300

    2-binary_tree_insert_right.c

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right-inserts a node as the left-child of another node
+ *
+ * @parent: parent
+ * @value: value
+ * Return: new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+	new->left = parent->right;
+	if (new->right)
+	new->right->parent = new;
+	parent->right = new;
+	return (new);
+}

8d1fc07555a2411200a1072a58311710594fdc91
kenkomu <komukenneth216@gmail.com>
2022-09-27 10:40:23 +0300

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,10 +9,11 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth;
 	if (tree == NULL)
 		return (0);
 
-	size_t depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
+	depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
 
 	return (depth);
 }

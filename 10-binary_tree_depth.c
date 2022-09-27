92d31bc7623559cbc2ecdfaeea3392db24f95b78
kenkomu <komukenneth216@gmail.com>
2022-09-27 10:39:04 +0300

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,11 +12,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	size_t leftDepth = binary_tree_depth(tree->left);
-	size_t rightDepth = binary_tree_depth(tree->right);
+	size_t depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
 
-	if (leftDepth > rightDepth)
-		return (leftDepth + 1);
-	else
-		return (rightDepth + 1);
+	return (depth);
 }

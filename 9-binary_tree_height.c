b5d072c6f4f650b9af6bae28184fae101c471e58
kenkomu <komukenneth216@gmail.com>
2022-09-27 09:37:32 +0300

    9-binary_tree_height.c

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height-measures the height of a binary tree
+ *
+ * @tree: tree to be measured
+ * Return: height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else if
+	{
+		int left_height = binary_tree_height(tree->left);
+		int right_height = binary_tree_height(tree->right);
+
+		if (left_height >= right_height)
+			return (left_height + 1);
+		else
+			return (right_height + 1);
+	}
+}

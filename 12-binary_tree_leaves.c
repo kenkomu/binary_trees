d604c8315653c242ef275ea7dab803d025a4dbd7
kenkomu <komukenneth216@gmail.com>
2022-09-27 11:20:50 +0300

    12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves-measure number of leaves of the tree
+ *
+ * @tree: tree to be measure
+ * Return: size_t
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (getLeafCount(tree->left) +
+			getLeafCount(tree->right));
+}

5ee4cd41d1886fa94e0be17553387218cc39e7e1
kenkomu <komukenneth216@gmail.com>
2022-09-27 11:11:04 +0300

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size-size of the tree
+ * binary_tree_size-measure size of the tree
  *
- * @tree: tree to be measured
+ * @tree: tree to be measure
  * Return: size_t
  */
 size_t binary_tree_size(const binary_tree_t *tree)
@@ -15,5 +15,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	sleft = binary_tree_size(tree->left);
 	sright = binary_tree_size(tree->right);
 
-	return (sleft + sright);
+	return (sleft + 1 + sright);
 }

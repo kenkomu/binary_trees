0af01db7f1138c58d7f57b89014f2968636b5027
kenkomu <komukenneth216@gmail.com>
2022-09-27 09:06:47 +0300

    7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,7 +10,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
+	func(tree->n);
 	binary_tree_preorder(tree->left, func);
-    func(tree->n);
+	binary_tree_preorder(tree->parent, func);
 	binary_tree_preorder(tree->right, func);
 }

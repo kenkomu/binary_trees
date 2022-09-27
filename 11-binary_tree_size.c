9bb7deca80f3327edd5d97ba8580f4cfe95afc8e
kenkomu <komukenneth216@gmail.com>
2022-09-27 10:53:50 +0300

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,7 +10,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t size;
 
 	if (tree == NULL)
-		return;
+		return (0);
 	else if
 	{
 		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));

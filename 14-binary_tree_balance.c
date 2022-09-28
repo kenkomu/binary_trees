55711b13bfd898d50cf44adfcd732489baf2cda9
kenkomu <komukenneth216@gmail.com>
2022-09-28 16:13:19 +0300

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,8 +7,13 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int lHeight, rHeight;
+
 	if (tree == NULL)
 		return (0);
-	return ((binary_tree_balance(tree->left) -
-		binary_tree_balance(tree->right)));
+
+	lHeight = tree->left ? (int) binary_tree_height(tree->left) : -1;
+	rHeight = tree->right ? (int) binary_tree_height(tree->right) : -1;
+
+	return (lHeight - rHeight);
 }
